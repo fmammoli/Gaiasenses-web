122eb8d913ea9c4660ea4651883de99de2825f54
Felipe Mammoli <fmammoli@gmail.com>
2025-06-13 19:51:35 -0300

    self closing dialog

--- app/[locale]/map3/receiver-dialog.tsx
+++ app/[locale]/map3/receiver-dialog.tsx
@@ -8,6 +8,8 @@ import {
 } from "@/components/ui/dialog";
 import { Gamepad } from "lucide-react";
 import Receiver2 from "../host/receiver2";
+import { useEffect, useRef, useState } from "react";
+import { useWebRTC } from "@/hooks/webrtc-context";
 
 function isMobile() {
   if (typeof navigator === "undefined") return false;
@@ -17,23 +19,39 @@ function isMobile() {
 }
 
 export default function ReceiverDialog() {
+  const dialogRef = useRef<HTMLDialogElement>(null);
+
+  const { dcOpen } = useWebRTC();
+  const [showDialog, setShowDialog] = useState(true);
+
+  useEffect(() => {
+    if (dcOpen) {
+      setShowDialog(false);
+    }
+  }, [dcOpen]);
+
   if (isMobile()) return null;
+
   return (
-    <Dialog>
-      <DialogTrigger asChild>
-        <div className="absolute top-[215px] right-0 z-10">
-          <div className="mr-[10px] mt-[10px]">
-            <button className="bg-white w-[29px] h-[29px] rounded-sm flex justify-center items-center hover:bg-gray-200">
-              <Gamepad width={22} height={22} strokeWidth={2.5} />
-            </button>
-          </div>
-        </div>
-      </DialogTrigger>
-      <DialogContent className="max-w-[75%]">
-        <DialogTitle>Receiver</DialogTitle>
-        <Receiver2 />
-        <DialogClose asChild></DialogClose>
-      </DialogContent>
-    </Dialog>
+    <>
+      {showDialog && (
+        <Dialog>
+          <DialogTrigger asChild>
+            <div className="absolute top-[215px] right-0 z-10">
+              <div className="mr-[10px] mt-[10px]">
+                <button className="bg-white w-[29px] h-[29px] rounded-sm flex justify-center items-center hover:bg-gray-200">
+                  <Gamepad width={22} height={22} strokeWidth={2.5} />
+                </button>
+              </div>
+            </div>
+          </DialogTrigger>
+          <DialogContent className="max-w-[75%]">
+            <DialogTitle>Receiver</DialogTitle>
+            <Receiver2 />
+            <DialogClose></DialogClose>
+          </DialogContent>
+        </Dialog>
+      )}
+    </>
   );
 }
